f95f52161644b8a6c5a71b8f9395ff39428f2070
SystemZ <hello@systemz.pl>
2017-11-06 14:36:05 +0100

    Autorestart serve process in dev

--- Taskfile.sh
+++ Taskfile.sh
@@ -1,13 +1,31 @@
 #!/bin/bash
 
+function kill_dev() {
+    PID1=$(ps aux | grep '/tmp/go' | awk '{print $2}')
+    PID2=$(ps aux | grep 'go run' | awk '{print $2}')
+    kill $PID1
+    kill $PID2
+}
+
+function sigint_handler() {
+    kill_dev
+    exit
+}
+
 function help {
     echo "$0 <task> <args>"
     echo "Tasks:"
     compgen -A function | cat -n
 }
 
-function dev {
-    go run *.go serve
+function serve {
+   trap sigint_handler SIGINT
+   while true; do
+       go run *.go serve &
+       # exclude all files with dots
+       inotifywait -e modify -e move -e create -e delete -e attrib -r `pwd` --exclude cache --exclude index.lock --exclude '/\..+' --exclude cache
+       kill_dev
+   done
 }
 
 function scan {
@@ -26,7 +44,7 @@ function prod {
 }
 
 function default {
-    dev
+    serve
 }
 
 TIMEFORMAT="Task completed in %3lR"
