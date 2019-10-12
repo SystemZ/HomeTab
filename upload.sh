f55d3b8afefbe1db017716066e941a143fe2a1cb
SystemZ <hello@systemz.pl>
2019-10-12 23:55:55 +0200

    Fix CI upload task - finally!

--- upload.sh
+++ upload.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
-mkdir -p /root/.config/rclone/
-echo $RCLONE_CONFIG | base64 -d > /root/.config/rclone/rclone.conf
 RAND_STR=$(pwgen -N 1 -s 32)
 URL="https://s3.eu-central-1.amazonaws.com/tmp.lvlup.pro/$RAND_STR/tasktab.apk"
 echo "Uploading to URL: $URL"
-rclone -vvv copyto app/build/outputs/apk/debug/app-debug.apk s3:tmp.lvlup.pro/$RAND_STR/tasktab.apk && curl -s \
+rclone -q copyto app/build/outputs/apk/debug/app-debug.apk s3:tmp.lvlup.pro/$RAND_STR/tasktab.apk && curl -s \
 --form-string "token=$PUSHOVER_TOKEN" \
 --form-string "user=$PUSHOVER_USER" \
 --form-string "title=New TaskTab Android version" \
