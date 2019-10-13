dde18fce4ddf8d795a05aed7379eba7e9298f193
SystemZ <hello@systemz.pl>
2019-10-13 17:29:01 +0200

    Background service refactor

--- app/src/main/java/pl/systemz/tasktab/StalkService.java
+++ app/src/main/java/pl/systemz/tasktab/StalkService.java
@@ -11,9 +11,11 @@ import android.util.Log;
 import android.widget.Toast;
 
 import com.google.gson.Gson;
-import com.hivemq.client.internal.mqtt.handler.disconnect.MqttDisconnectEvent;
 import com.hivemq.client.mqtt.MqttClient;
 import com.hivemq.client.mqtt.mqtt3.Mqtt3AsyncClient;
+import com.hivemq.client.mqtt.mqtt3.message.connect.connack.Mqtt3ConnAck;
+import com.hivemq.client.mqtt.mqtt3.message.publish.Mqtt3Publish;
+import com.hivemq.client.mqtt.mqtt3.message.subscribe.suback.Mqtt3SubAck;
 
 import java.nio.charset.StandardCharsets;
 
@@ -34,6 +36,8 @@ public class StalkService extends Service {
     IBinder mBinder;      // interface for clients that bind
     boolean mAllowRebind; // indicates whether onRebind should be used
 
+    Mqtt3AsyncClient mqClient;
+
     @Override
     public void onCreate() {
         // The service is being created
@@ -46,18 +50,17 @@ public class StalkService extends Service {
         Log.d(TAG, "onStart, register events");
 
         // Hopefully your alarm will have a lower frequency than this!
-//        AlarmManager alarmMgr;
-//        alarmMgr = (AlarmManager)this.getSystemService(Context.ALARM_SERVICE);
-//        pi = PendingIntent.getBroadcast(this, 11, new Intent("pl.systemz.stalk.GPSSETTINGS"), 0);
-//        am.setInexactRepeating(AlarmManager.RTC_WAKEUP, 0, TWENTYFIVE_SECONDS, pi);
+        // AlarmManager alarmMgr;
+        // alarmMgr = (AlarmManager)this.getSystemService(Context.ALARM_SERVICE);
+        // pi = PendingIntent.getBroadcast(this, 11, new Intent("pl.systemz.stalk.GPSSETTINGS"), 0);
+        // am.setInexactRepeating(AlarmManager.RTC_WAKEUP, 0, TWENTYFIVE_SECONDS, pi);
 
-//        alarmMgr.setInexactRepeating(AlarmManager.ELAPSED_REALTIME_WAKEUP,
-//                SystemClock.elapsedRealtime() + 60*1000,
-//                60*1000, alarmIntent);
+        // alarmMgr.setInexactRepeating(AlarmManager.ELAPSED_REALTIME_WAKEUP,
+        //         SystemClock.elapsedRealtime() + 60*1000,
+        //         60*1000, alarmIntent);
 
-//                SystemClock.elapsedRealtime() + AlarmManager.INTERVAL_HALF_HOUR,
-//                AlarmManager.INTERVAL_HALF_HOUR, alarmIntent);
-
+        // SystemClock.elapsedRealtime() + AlarmManager.INTERVAL_HALF_HOUR,
+        // AlarmManager.INTERVAL_HALF_HOUR, alarmIntent);
 
         IntentFilter intentFilter = new IntentFilter();
         intentFilter.addAction(Intent.ACTION_SCREEN_ON);
@@ -68,9 +71,8 @@ public class StalkService extends Service {
         this.antenna = new Antenna();
         registerReceiver(antenna, intentFilter);
 
-
-        // to listen for MQTT messages
-        Mqtt3AsyncClient client = MqttClient
+        // connect to MQTT server
+        mqClient = MqttClient
                 .builder()
                 .useMqttVersion3()
                 .identifier("tasktab-android")
@@ -88,62 +90,14 @@ public class StalkService extends Service {
                 //.useSslWithDefaultConfig()
                 .buildAsync();
 
-        client.connectWith()
+        // login to MQTT server
+        mqClient.connectWith()
                 .simpleAuth()
                 .username("tasktab:tasktab-android")
                 .password("changeme".getBytes())
                 .applySimpleAuth()
                 .send()
-                .whenComplete((connAck, throwable) -> {
-                    if (throwable != null) {
-                        // handle failure
-                        Log.d(TAG, "Failure connecting to MQTT server");
-                    } else {
-                        // setup subscribes or start publishing
-                        Log.d(TAG, "Connected to MQTT server");
-                        client.subscribeWith()
-                                .topicFilter("tasktab")
-                                .callback(publish -> {
-                                    // Process the received message
-                                    Log.d(TAG, "Got a new msg via MQTT");
-                                    String msgTxt = new String(publish.getPayloadAsBytes(), StandardCharsets.UTF_8);
-                                    // deserialize
-                                    // {"type":"notification","msg":"testzz",id:1}
-                                    Gson gson = new Gson();
-                                    MqttMsg msgObj = gson.fromJson(msgTxt, MqttMsg.class);
-
-                                    NotificationCompat.Builder builder = new NotificationCompat.Builder(this, "tasktab-counters")
-                                            .setSmallIcon(R.drawable.ic_access_time_black_24dp)
-                                            .setContentTitle(msgObj.getMsg())
-                                            .setContentText("In progress...")
-                                            .setUsesChronometer(true)
-                                            .setWhen(currentTimeMillis())
-                                            //.setOngoing(true)
-//                                            .setContentIntent(tapIntent)
-//                .addAction(R.mipmap.ic_launcher, "Stop", stopActionIntent)
-                                            .setColor(Color.GREEN)
-                                            //.setAutoCancel(true)
-                                            .setVibrate(new long[]{150})
-                                            //.setSound(Settings.System.DEFAULT_NOTIFICATION_URI)
-                                            .setPriority(NotificationCompat.PRIORITY_DEFAULT);
-
-                                    NotificationManagerCompat notificationManager = NotificationManagerCompat.from(this);
-                                    // notificationId is a unique int for each notification that you must define
-                                    notificationManager.notify(msgObj.getId(), builder.build());
-
-                                })
-                                .send()
-                                .whenComplete((subAck, throwable2) -> {
-                                    if (throwable2 != null) {
-                                        // Handle failure to subscribe
-                                        Log.d(TAG, "Subscribe error");
-                                    } else {
-                                        // Handle successful subscription, e.g. logging or incrementing a metric
-                                        Log.d(TAG, "Subscribe OK");
-                                    }
-                                });
-                    }
-                });
+                .whenComplete(this::mqConnectionComplete);
     }
 
     @Override
@@ -156,7 +110,7 @@ public class StalkService extends Service {
 
     @Override
     public IBinder onBind(Intent intent) {
-        // A client is binding to the service with bindService()
+        // A mqClient is binding to the service with bindService()
         return mBinder;
     }
 
@@ -168,7 +122,7 @@ public class StalkService extends Service {
 
     @Override
     public void onRebind(Intent intent) {
-        // A client is binding to the service with bindService(),
+        // A mqClient is binding to the service with bindService(),
         // after onUnbind() has already been called
     }
 
@@ -177,6 +131,65 @@ public class StalkService extends Service {
         // The service is no longer used and is being destroyed
         Toast.makeText(this, "Stalk Service stopped", Toast.LENGTH_LONG).show();
         Log.d(TAG, "onDestroy");
+    }
+
+    // MQTT related below
+    //
+    private void mqConnectionComplete(Mqtt3ConnAck connAck, Throwable throwable) {
+        if (throwable != null) {
+            // handle failure
+            Log.d(TAG, "Failure connecting to MQTT server");
+        } else {
+            // setup subscribes or start publishing
+            Log.d(TAG, "Connected to MQTT server");
+            mqClient.subscribeWith()
+                    .topicFilter("tasktab")
+                    // Process the received message
+                    .callback(this::newMqMsg)
+                    .send()
+                    .whenComplete(StalkService::subscribeComplete);
+        }
+    }
+
+    private static void subscribeComplete(Mqtt3SubAck subAck, Throwable throwable2) {
+        if (throwable2 != null) {
+            // Handle failure to subscribe
+            Log.d(TAG, "Subscribe error");
+        } else {
+            // Handle successful subscription, e.g. logging or incrementing a metric
+            Log.d(TAG, "Subscribe OK");
+        }
+    }
+
+    private void newMqMsg(Mqtt3Publish publish) {
+        Log.d(TAG, "Got a new msg via MQTT");
+        String msgTxt = new String(publish.getPayloadAsBytes(), StandardCharsets.UTF_8);
+        // deserialize
+        // {"type":"notification","msg":"testzz",id:1}
+        Gson gson = new Gson();
+        MqttMsg msgObj = gson.fromJson(msgTxt, MqttMsg.class);
+
+        newNotification(msgObj);
+    }
+
+    private void newNotification(MqttMsg msgObj) {
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this, "tasktab-counters")
+                .setSmallIcon(R.drawable.ic_access_time_black_24dp)
+                .setContentTitle(msgObj.getMsg())
+                .setContentText("In progress...")
+                .setUsesChronometer(true)
+                .setWhen(currentTimeMillis())
+                //.setOngoing(true)
+                //.setContentIntent(tapIntent)
+                //.addAction(R.mipmap.ic_launcher, "Stop", stopActionIntent)
+                .setColor(Color.GREEN)
+                //.setAutoCancel(true)
+                .setVibrate(new long[]{150})
+                //.setSound(Settings.System.DEFAULT_NOTIFICATION_URI)
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT);
+        NotificationManagerCompat notificationManager = NotificationManagerCompat.from(this);
+        // notificationId is a unique int for each notification that you must define
+        notificationManager.notify(msgObj.getId(), builder.build());
     }
 
 }
