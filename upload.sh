685155f19bff80cb58c1640eb0f78a19949a9c22
SystemZ <hello@systemz.pl>
2019-10-14 14:52:11 +0200

    Build release version via CI

--- upload.sh
+++ upload.sh
@@ -2,7 +2,7 @@
 RAND_STR=$(pwgen -N 1 -s 32)
 URL="https://s3.eu-central-1.amazonaws.com/tmp.lvlup.pro/$RAND_STR/tasktab.apk"
 echo "Uploading to URL: $URL"
-rclone -q copyto app/build/outputs/apk/debug/app-debug.apk s3:tmp.lvlup.pro/$RAND_STR/tasktab.apk && curl -s \
+rclone -q copyto app/build/outputs/apk/release/app-release.apk s3:tmp.lvlup.pro/$RAND_STR/tasktab.apk && curl -s \
 --form-string "token=$PUSHOVER_TOKEN" \
 --form-string "user=$PUSHOVER_USER" \
 --form-string "title=New TaskTab Android version" \
