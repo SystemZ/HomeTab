ab1d4e852133f931ac298dcd73d9db2760df36db
SystemZ <hello@systemz.pl>
2019-10-19 15:46:00 +0200

    Remove argocd webhook

--- web/main.go
+++ web/main.go
@@ -68,7 +68,6 @@ func StartWebInterface() {
 	r.HandleFunc("/api/v1/counter/{id}/stop", ApiCounterStop)
 	// Webhooks
 	r.HandleFunc("/wh/gitlab", WebhookGitlab)
-	r.HandleFunc("/wh/argocd", WebhookArgocd)
 	// start internal http server with logging
 	loggedRouter := handlers.LoggingHandler(os.Stdout, r)
 	log.Println("HTTP server started on :3000")
