9cd203da5b8aefbcfbe718ff0743543011924efc
SystemZ <hello@systemz.pl>
2019-12-19 16:20:09 +0100

    Frontend main template cleanup

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,5 +1,5 @@
 <template>
-    <v-app id="inspire">
+    <v-app>
         <v-navigation-drawer
                 v-model="drawer"
                 app
@@ -13,24 +13,16 @@
                         <v-list-item-title>Home</v-list-item-title>
                     </v-list-item-content>
                 </v-list-item>
-                <v-list-item link>
-                    <v-list-item-action>
-                        <v-icon>mdi-contact-mail</v-icon>
-                    </v-list-item-action>
-                    <v-list-item-content>
-                        <v-list-item-title>Contact</v-list-item-title>
-                    </v-list-item-content>
-                </v-list-item>
             </v-list>
         </v-navigation-drawer>
 
         <v-app-bar
                 app
-                color="indigo"
+                color="green darken-1"
                 dark
         >
             <v-app-bar-nav-icon @click.stop="drawer = !drawer"/>
-            <v-toolbar-title>Application</v-toolbar-title>
+            <v-toolbar-title>TaskTab</v-toolbar-title>
         </v-app-bar>
 
         <v-content>
@@ -40,25 +32,14 @@
             >
             </v-container>
         </v-content>
-        <v-footer
-                color="indigo"
-                app
-        >
-            <span class="white--text">&copy; 2019</span>
-        </v-footer>
     </v-app>
 </template>
 
 <script lang="ts">
     import Vue from 'vue';
-    import HelloWorld from './components/HelloWorld.vue';
 
     export default Vue.extend({
         name: 'App',
-
-        components: {
-            HelloWorld,
-        },
 
         data: () => ({
             drawer: null,
