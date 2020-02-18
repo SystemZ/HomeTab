05a5aa54c84675e7923e1892588ab00027d7d9f4
SystemZ <hello@systemz.pl>
2020-02-19 00:36:34 +0100

    Migrations fix (kinda)

--- migrations/1_init_up.sql
+++ migrations/1_init_up.sql
@@ -1,37 +1,59 @@
--- MySQL dump 10.13  Distrib 5.7.27, for Linux (x86_64)
+-- MariaDB dump 10.17  Distrib 10.5.1-MariaDB, for debian-linux-gnu (x86_64)
 --
 -- Host: localhost    Database: dev
 -- ------------------------------------------------------
--- Server version	5.7.27
+-- Server version	10.5.1-MariaDB-1:10.5.1+maria~bionic
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT = @@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS = @@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION = @@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE = @@TIME_ZONE */;
+/*!40103 SET TIME_ZONE = '+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS = @@UNIQUE_CHECKS, UNIQUE_CHECKS = 0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS = @@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS = 0 */;
+/*!40101 SET @OLD_SQL_MODE = @@SQL_MODE, SQL_MODE = 'NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES = @@SQL_NOTES, SQL_NOTES = 0 */;
 
 --
 -- Table structure for table `file_tags`
 --
 
 DROP TABLE IF EXISTS `file_tags`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET @saved_cs_client = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `file_tags`
 (
     `id`         int(11) NOT NULL AUTO_INCREMENT,
+    `user_id`    int(11)  DEFAULT NULL,
     `file_id`    int(11)  DEFAULT NULL,
     `tag_id`     int(11)  DEFAULT NULL,
     `created_at` datetime DEFAULT NULL,
     `updated_at` datetime DEFAULT NULL,
     `deleted_at` datetime DEFAULT NULL,
     PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE = InnoDB
+  DEFAULT CHARSET = latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `file_users`
+--
+
+DROP TABLE IF EXISTS `file_users`;
+/*!40101 SET @saved_cs_client = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `file_users`
+(
+    `id`         int(11) NOT NULL AUTO_INCREMENT,
+    `file_id`    int(11)  DEFAULT NULL,
+    `user_id`    int(11)  DEFAULT NULL,
+    `created_at` datetime DEFAULT NULL,
+    `updated_at` datetime DEFAULT NULL,
+    `deleted_at` datetime DEFAULT NULL,
+    PRIMARY KEY (`id`)
+) ENGINE = InnoDB
+  DEFAULT CHARSET = latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -39,26 +61,26 @@ CREATE TABLE `file_tags`
 --
 
 DROP TABLE IF EXISTS `files`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET @saved_cs_client = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `files`
 (
     `id`         int(11) NOT NULL AUTO_INCREMENT,
-    `user_id`    int(11)    DEFAULT NULL,
-    `file_name`  varchar(255) CHARACTER SET utf8mb4 DEFAULT NULL,
+    `file_name`  varchar(255) CHARACTER SET utf8mb4  DEFAULT NULL,
     `file_path`  varchar(4096) CHARACTER SET utf8mb4 DEFAULT NULL,
-    `size_b`     int(11)    DEFAULT NULL,
-    `mime_id`    int(11)    DEFAULT NULL,
-    `phash_a`    bigint(16) DEFAULT NULL,
-    `phash_b`    bigint(16) DEFAULT NULL,
-    `phash_c`    bigint(16) DEFAULT NULL,
-    `phash_d`    bigint(16) DEFAULT NULL,
-    `sha256`     char(64)   DEFAULT NULL,
-    `created_at` datetime   DEFAULT NULL,
-    `updated_at` datetime   DEFAULT NULL,
-    `deleted_at` datetime   DEFAULT NULL,
+    `size_b`     int(11)                             DEFAULT NULL,
+    `mime_id`    int(11)                             DEFAULT NULL,
+    `phash_a`    bigint(16)                          DEFAULT NULL,
+    `phash_b`    bigint(16)                          DEFAULT NULL,
+    `phash_c`    bigint(16)                          DEFAULT NULL,
+    `phash_d`    bigint(16)                          DEFAULT NULL,
+    `sha256`     char(64)                            DEFAULT NULL,
+    `created_at` datetime                            DEFAULT NULL,
+    `updated_at` datetime                            DEFAULT NULL,
+    `deleted_at` datetime                            DEFAULT NULL,
     PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE = InnoDB
+  DEFAULT CHARSET = latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -66,7 +88,7 @@ CREATE TABLE `files`
 --
 
 DROP TABLE IF EXISTS `gomigrate`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET @saved_cs_client = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `gomigrate`
 (
@@ -74,7 +96,8 @@ CREATE TABLE `gomigrate`
     `migration_id` bigint(20) NOT NULL,
     PRIMARY KEY (`id`),
     UNIQUE KEY `migration_id` (`migration_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE = InnoDB
+  DEFAULT CHARSET = latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -82,7 +105,7 @@ CREATE TABLE `gomigrate`
 --
 
 DROP TABLE IF EXISTS `mimes`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET @saved_cs_client = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `mimes`
 (
@@ -92,7 +115,8 @@ CREATE TABLE `mimes`
     `updated_at` datetime     DEFAULT NULL,
     `deleted_at` datetime     DEFAULT NULL,
     PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE = InnoDB
+  DEFAULT CHARSET = latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -100,42 +124,66 @@ CREATE TABLE `mimes`
 --
 
 DROP TABLE IF EXISTS `tags`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET @saved_cs_client = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `tags`
 (
     `id`         int(11) NOT NULL AUTO_INCREMENT,
     `tag`        varchar(255) CHARACTER SET utf8mb4 DEFAULT NULL,
-    `created_at` datetime DEFAULT NULL,
-    `updated_at` datetime DEFAULT NULL,
-    `deleted_at` datetime DEFAULT NULL,
+    `created_at` datetime                           DEFAULT NULL,
+    `updated_at` datetime                           DEFAULT NULL,
+    `deleted_at` datetime                           DEFAULT NULL,
     PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE = InnoDB
+  DEFAULT CHARSET = latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+--
+-- Table structure for table `users`
+--
 
--- Dump completed on 2020-02-14 11:09:57
+DROP TABLE IF EXISTS `users`;
+/*!40101 SET @saved_cs_client = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `users`
+(
+    `id`                 int(10) unsigned NOT NULL AUTO_INCREMENT,
+    `username`           varchar(24)      NOT NULL,
+    `email`              varchar(254)     NOT NULL,
+    `hash`               char(60)         NOT NULL COMMENT 'bcrypt.GenerateFromPassword',
+    `default_project_id` int(10) unsigned NOT NULL DEFAULT 0,
+    `created_at`         datetime         NOT NULL,
+    `updated_at`         datetime         NOT NULL,
+    PRIMARY KEY (`id`),
+    UNIQUE KEY `username` (`username`),
+    UNIQUE KEY `email` (`email`)
+) ENGINE = InnoDB
+  DEFAULT CHARSET = latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+/*!40103 SET TIME_ZONE = @OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE = @OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS = @OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS = @OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT = @OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS = @OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION = @OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES = @OLD_SQL_NOTES */;
+
+-- Dump completed on 2020-02-18 23:05:00
 
 
 DROP
-FUNCTION IF EXISTS HAMMINGDISTANCE;
+    FUNCTION IF EXISTS HAMMINGDISTANCE;
 
 CREATE
-FUNCTION HAMMINGDISTANCE(
-  A0 BIGINT, A1 BIGINT, A2 BIGINT, A3 BIGINT,
-  B0 BIGINT, B1 BIGINT, B2 BIGINT, B3 BIGINT
+    FUNCTION HAMMINGDISTANCE(
+    A0 BIGINT, A1 BIGINT, A2 BIGINT, A3 BIGINT,
+    B0 BIGINT, B1 BIGINT, B2 BIGINT, B3 BIGINT
 )
-RETURNS INT DETERMINISTIC
-RETURN
-  BIT_COUNT(A0 ^ B0) +
-  BIT_COUNT(A1 ^ B1) +
-  BIT_COUNT(A2 ^ B2) +
-  BIT_COUNT(A3 ^ B3);
+    RETURNS INT DETERMINISTIC
+    RETURN
+            BIT_COUNT(A0 ^ B0) +
+            BIT_COUNT(A1 ^ B1) +
+            BIT_COUNT(A2 ^ B2) +
+            BIT_COUNT(A3 ^ B3);
