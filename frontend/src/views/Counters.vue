aff1a7be051a5471bba2bca2b2cec0b0fcbee839
SystemZ <hello@systemz.pl>
2020-02-22 21:14:31 +0100

    Reformat Counters view

--- frontend/src/views/Counters.vue
+++ frontend/src/views/Counters.vue
@@ -2,7 +2,7 @@
     <v-container fluid>
         <v-card class="ma-5">
             <v-card-title>
-                Counters
+                All counters
                 <v-spacer></v-spacer>
                 <v-row>
                     <v-switch v-model="searchPro" label="Pro" color="green" class="mr-5"></v-switch>
@@ -45,106 +45,107 @@
 </template>
 
 <script>
-    import axios from "axios";
+  import axios from 'axios'
 
-    export default {
-        name: 'counters',
-        data() {
-            return {
-                search: '',
-                searchPro: false,
-                searchTimeout: null,
-                //
-                countersLoading: true,
-                tableOptions: {},
-                totalCounters: 0,
-                headers: [
-                    {
-                        text: 'ID',
-                        align: 'left',
-                        sortable: true,
-                        value: 'id',
-                    },
-                    {text: 'Title', value: 'name'},
-                    {text: 'Tags', value: 'tags'},
-                    {text: 'Time', value: 'seconds'},
-                    // {text: 'Created at', value: 'createdAt'},
-                ],
-                counters: [],
-                prevItemsPerPage: 0,
-                prevPage: 0,
-            }
-        },
-        methods: {
-            authConfig() {
-                return {headers: {Authorization: "Bearer " + localStorage.getItem(this.lsToken)}}
-            },
-            toCounter(item) {
-                this.$router.push({name: 'counter', params: {id: item.id}})
-            },
-            getCounters(pagination, resetPagination) {
-                let vm = this
-                vm.countersLoading = true
-                let lastId = 0
-                let queryType = "next"
-                if (vm.counters.length > 0) {
-                    if (pagination !== undefined && pagination.page > vm.prevPage) {
-                        // next page button
-                        lastId = vm.counters[vm.counters.length - 1].id
-                    } else if (resetPagination) {
-                        // search box handling
-                        // we don't want to skip records with ID bigger than 1
-                        lastId = 0
-                        vm.tableOptions.page = 1
-                    } else {
-                        // prev page button
-                        lastId = vm.counters[0].id
-                        queryType = "prev"
-                    }
-                }
-
-                let itemsPerPage = vm.prevItemsPerPage
-                if (pagination !== undefined) {
-                    vm.prevPage = pagination.page
-                    if (pagination.itemsPerPage !== vm.prevItemsPerPage) {
-                        lastId = 0
-                    }
-                    vm.prevItemsPerPage = pagination.itemsPerPage
-                    itemsPerPage = pagination.itemsPerPage
-                }
-
-                let searchQuery = "%" + vm.search + "%"
-                if (vm.searchPro) {
-                    searchQuery = vm.search
-                }
-
-                let rawUrl = vm.apiUrl + "/api/v1/counter-page?limit=" + itemsPerPage + "&" + queryType + "Id=" + lastId
-                let url = encodeURI(rawUrl)
-                axios.post(url, {q: searchQuery}, vm.authConfig())
-                    .then((res) => {
-                        vm.countersLoading = false
-                        vm.counters = res.data.counters
-                        vm.totalCounters = res.data.pagination.allRecords
-                    })
-                    .catch(function (err) {
-                        if (err.response.status === 401) {
-                            console.log("logged out")
-                            vm.$root.$emit("sessionExpired")
-                        } else if (err.response.status === 400) {
-                            console.log("empty result / wrong request")
-                            vm.countersLoading = false
-                            // make table empty as backend result is empty
-                            vm.counters = []
-                            vm.totalCounters = 0
-                        } else {
-                            console.log("something wrong")
-                        }
-                    })
-            },
-            doSearch() {
-                clearTimeout(this.searchTimeout)
-                this.searchTimeout = setTimeout(this.getCounters, 500, undefined, true)
-            },
+  export default {
+    name: 'counters',
+    data () {
+      return {
+        search: '',
+        searchPro: false,
+        searchTimeout: null,
+        //
+        countersLoading: true,
+        tableOptions: {},
+        totalCounters: 0,
+        headers: [
+          {
+            text: 'ID',
+            align: 'left',
+            sortable: true,
+            value: 'id',
+          },
+          {text: 'Title', value: 'name'},
+          {text: 'Tags', value: 'tags'},
+          {text: 'Time', value: 'seconds'},
+          // {text: 'Created at', value: 'createdAt'},
+        ],
+        counters: [],
+        prevItemsPerPage: 0,
+        prevPage: 0,
+        newCounterDialogShow: false,
+      }
+    },
+    methods: {
+      authConfig () {
+        return {headers: {Authorization: 'Bearer ' + localStorage.getItem(this.lsToken)}}
+      },
+      toCounter (item) {
+        this.$router.push({name: 'counter', params: {id: item.id}})
+      },
+      getCounters (pagination, resetPagination) {
+        let vm = this
+        vm.countersLoading = true
+        let lastId = 0
+        let queryType = 'next'
+        if (vm.counters.length > 0) {
+          if (pagination !== undefined && pagination.page > vm.prevPage) {
+            // next page button
+            lastId = vm.counters[vm.counters.length - 1].id
+          } else if (resetPagination) {
+            // search box handling
+            // we don't want to skip records with ID bigger than 1
+            lastId = 0
+            vm.tableOptions.page = 1
+          } else {
+            // prev page button
+            lastId = vm.counters[0].id
+            queryType = 'prev'
+          }
         }
+
+        let itemsPerPage = vm.prevItemsPerPage
+        if (pagination !== undefined) {
+          vm.prevPage = pagination.page
+          if (pagination.itemsPerPage !== vm.prevItemsPerPage) {
+            lastId = 0
+          }
+          vm.prevItemsPerPage = pagination.itemsPerPage
+          itemsPerPage = pagination.itemsPerPage
+        }
+
+        let searchQuery = '%' + vm.search + '%'
+        if (vm.searchPro) {
+          searchQuery = vm.search
+        }
+
+        let rawUrl = vm.apiUrl + '/api/v1/counter-page?limit=' + itemsPerPage + '&' + queryType + 'Id=' + lastId
+        let url = encodeURI(rawUrl)
+        axios.post(url, {q: searchQuery}, vm.authConfig())
+          .then((res) => {
+            vm.countersLoading = false
+            vm.counters = res.data.counters
+            vm.totalCounters = res.data.pagination.allRecords
+          })
+          .catch(function (err) {
+            if (err.response.status === 401) {
+              console.log('logged out')
+              vm.$root.$emit('sessionExpired')
+            } else if (err.response.status === 400) {
+              console.log('empty result / wrong request')
+              vm.countersLoading = false
+              // make table empty as backend result is empty
+              vm.counters = []
+              vm.totalCounters = 0
+            } else {
+              console.log('something wrong')
+            }
+          })
+      },
+      doSearch () {
+        clearTimeout(this.searchTimeout)
+        this.searchTimeout = setTimeout(this.getCounters, 500, undefined, true)
+      },
     }
+  }
 </script>
