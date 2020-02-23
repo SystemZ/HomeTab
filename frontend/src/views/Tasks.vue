a89c22ae62ddc4d4766c1dbee1e249446a5bfb0b
Paulina Jabłońska <paulina@lvlup.pro>
2020-02-23 15:02:17 +0100

    Workaround to fix checkboxes

--- frontend/src/views/Tasks.vue
+++ frontend/src/views/Tasks.vue
@@ -50,7 +50,8 @@
             </v-col>
         </v-row>
         <v-row>
-            <v-col cols="12" xs="12" md="4">
+            <v-col md="3"></v-col>
+            <v-col cols="12" xs="12" md="6">
                 <v-text-field
                         placeholder="Buy a yacht"
                         solo
@@ -59,30 +60,18 @@
                         @keydown.enter.native="addTask"
                 >
                 </v-text-field>
-            </v-col>
-            <v-col>
                 <v-btn
                         block
                         color="success"
-                        v-if="$vuetify.breakpoint.smAndDown"
-                        @click.native="addTask"
-                >
-                    Add
-                </v-btn>
-                <v-btn
-                        color="success"
-                        large
-                        height="48px"
-                        v-else
                         @click.native="addTask"
                 >
                     Add
                 </v-btn>
             </v-col>
         </v-row>
-        <v-row></v-row>
         <v-row>
-            <v-col>
+            <v-col md="3"></v-col>
+            <v-col cols="12" xs="12" md="6">
                 <v-card
                         class="mx-auto"
                 >
@@ -104,30 +93,31 @@
                     </v-toolbar>
                     <v-list
                             subheader
-                            one-line
                     >
                         <v-subheader>Tasks</v-subheader>
-                        <v-list-item-group multiple>
-                            <!--  TODO fix checkbox after deleting tasks -->
+                        <template v-for="(task, i) in tasks">
                             <v-list-item
-                                    v-for="(task, i) in tasks"
-                                    :key="i"
-                                    inactive
+                                    :key="`${i}-${task.title}`"
                             >
-                                <template v-slot:default>
-                                    <v-list-item-action>
-                                        <v-checkbox
-                                                v-model="task.selected"
-                                                color="green darken-2"
-                                        ></v-checkbox>
-                                    </v-list-item-action>
-
-                                    <v-list-item-content @click.stop="showDialog(task)">
-                                        <v-list-item-title v-text="task.title"></v-list-item-title>
-                                    </v-list-item-content>
-                                </template>
+                                <v-list-item-action>
+                                    <v-checkbox
+                                            v-model="task.selected"
+                                            :color="task.selected && 'green darken-2' || 'grey'"
+                                    >
+                                        <template v-slot:label>
+                                            <div
+                                                    :class="task.selected && 'grey--text' || 'black--text'"
+                                                    class="ml-4"
+                                                    v-text="task.title"
+                                            >
+                                            </div>
+                                        </template>
+                                    </v-checkbox>
+                                </v-list-item-action>
+                                <v-spacer></v-spacer>
+                                <v-icon @click.stop="showDialog(task)">mdi-information-outline</v-icon>
                             </v-list-item>
-                        </v-list-item-group>
+                        </template>
                     </v-list>
                 </v-card>
             </v-col>
@@ -152,7 +142,7 @@
         methods: {
             addTask() {
                 if (this.newTaskTitle !== "") {
-                    this.tasks.push({"title": this.newTaskTitle});
+                    this.tasks.push({"title": this.newTaskTitle, "selected": false,});
                     this.newTaskTitle = ""
                 }
             },
@@ -163,6 +153,7 @@
             },
             showDialog(task) {
                 this.taskTitleInDialog = task.title
+                this.tasks.selected = false
                 this.dialog = true
             },
         },
