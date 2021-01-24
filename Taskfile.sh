a23a32acb6163827d8f188fc3980bcefb3b82f57
Michał Frąckiewicz <hello@systemz.pl>
2021-01-24 08:34:52 +0000

    Task for local docker host deploy

--- Taskfile.sh
+++ Taskfile.sh
@@ -18,6 +18,17 @@ function dev-dump-schema() {
   docker-compose exec -T db /bin/sh -c "/usr/bin/mysqldump -udev -pdev --no-data dev" | grep -v "Using a password on the command line interface can be insecure" | sed 's/ AUTO_INCREMENT=[0-9]*//g' >migrations/0.sql
 }
 
+function deploy-docker() {
+  [ -z "$1" ] && echo "Host not provided" >&2 && exit 1
+  rm -rf new
+  cd frontend || exit
+  yarn build
+  mv dist ../new
+  cd ../
+  CGO_ENABLED=0 go build -o tasktab
+  docker build -t tasktab . && docker save tasktab | bzip2 | pv | ssh $1 'bunzip2 | docker load'
+}
+
 function default() {
   help
 }
